18e2dc7c0614f7f5146224230361117a67f1794e
e-7281998 <jejjcs43@naver.com>
2023-05-10 12:30:44 +0900

    [AOP] select로 시작하는 메소드 수행시 log 출력

---
+++ src/main/java/com/shinhan/education/LogAdvice.java
@@ -0,0 +1,25 @@
+package com.shinhan.education;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAdvice {
+	
+ 	@Pointcut("execution(* select*(..))")
+	public void cut1() {} 
+	
+	@Around("cut1()")
+	public Object around(ProceedingJoinPoint jp) throws Throwable {
+		System.out.println("-- cut1 주 업무 시작 전 (calss 이름) "+ jp.getTarget().getClass().getName());
+		Object obj = jp.proceed();
+		System.out.println("-- cut1 주 업무 시작 후 (함수 이름) "+ jp.getSignature().getName());
+		return obj;
+	}
+	
+}
